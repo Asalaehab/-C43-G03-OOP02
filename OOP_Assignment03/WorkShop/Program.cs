ff5da536742a3aac25a70d75b9e5f98def089775
Asala Ehab <105808104+Asalaehab@users.noreply.github.com>
2025-01-20 01:26:12 +0200

    Add files via upload

---
+++ OOP_Assignment03/WorkShop/Program.cs
@@ -0,0 +1,35 @@
+﻿using WorkShop.Overload;
+
+namespace WorkShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag;
+            int size;
+
+            do
+            {
+                Console.Write("Enter Size");
+                flag=int.TryParse(Console.ReadLine(), out size);
+            } while (! flag);
+
+            Employee[] employees = Employee.CreateEmployee(size);
+            
+
+            Employee.InsertEmployees(employees);
+            Console.Clear();
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+
+
+
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Add(2, 2));
+
+        }
+    }
+}
